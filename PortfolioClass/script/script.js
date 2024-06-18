33608c27c41e2785746b0380c5fe7c9ed8d19995
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-06-18 04:25:00 +0330

    Fix Bug

--- PortfolioClass/script/script.js
+++ PortfolioClass/script/script.js
@@ -25,18 +25,18 @@ $(document).ready(function(){
   })
   $('.left').click(function(){
     if($('.one').attr('src') == images[0]){
-        $('.one').attr('src',images[1])
-        $('.two').attr('src',images[2])
+        $('.one').attr('src',images[2])
+        $('.two').attr('src',images[1])
         $('.three').attr('src',images[0])
     } 
     else if ($('.one').attr('src') == images[1]){
-        $('.one').attr('src',images[2])
-        $('.two').attr('src',images[0])
+        $('.one').attr('src',images[0])
+        $('.two').attr('src',images[2])
         $('.three').attr('src',images[1])
     } 
     else if ($('.one').attr('src') == images[2]){
-        $('.one').attr('src',images[0])
-        $('.two').attr('src',images[1])
+        $('.one').attr('src',images[1])
+        $('.two').attr('src',images[0])
         $('.three').attr('src',images[2])
     }
 })
