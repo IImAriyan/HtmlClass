8b475fd925062f34d4b36bb81837afe1f0e8093d
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-06-18 04:16:36 +0330

    Done Project

--- PortfolioClass/script/script.js
+++ PortfolioClass/script/script.js
@@ -1,3 +1,43 @@
-function testClick() {
-    alert("hello")
-}
+$(document).ready(function(){
+  images= [
+    'assets/1.png',
+    'assets/2.png',
+    'assets/3.png'
+  ]  
+
+
+  $('.right').click(function(){
+        if($('.one').attr('src') == images[0]){
+            $('.one').attr('src',images[1])
+            $('.two').attr('src',images[2])
+            $('.three').attr('src',images[0])
+        } 
+        else if ($('.one').attr('src') == images[1]){
+            $('.one').attr('src',images[2])
+            $('.two').attr('src',images[0])
+            $('.three').attr('src',images[1])
+        } 
+        else if ($('.one').attr('src') == images[2]){
+            $('.one').attr('src',images[0])
+            $('.two').attr('src',images[1])
+            $('.three').attr('src',images[2])
+        }
+  })
+  $('.left').click(function(){
+    if($('.one').attr('src') == images[0]){
+        $('.one').attr('src',images[1])
+        $('.two').attr('src',images[2])
+        $('.three').attr('src',images[0])
+    } 
+    else if ($('.one').attr('src') == images[1]){
+        $('.one').attr('src',images[2])
+        $('.two').attr('src',images[0])
+        $('.three').attr('src',images[1])
+    } 
+    else if ($('.one').attr('src') == images[2]){
+        $('.one').attr('src',images[0])
+        $('.two').attr('src',images[1])
+        $('.three').attr('src',images[2])
+    }
+})
+})
